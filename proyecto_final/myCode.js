f21b4be1678dc07e9e60f8c9d73dbd6e2532f62e
lucas1992 <lucas.etcheverry28@gmail.com>
2021-11-10 22:19:52 -0300

    Entrega 6: Arrays. Incluye: --> Desafío: Incorporar Arrays --> Ordenar un array de objetos --> Primera Entrega del Proyecto Final

--- proyecto_final/myCode.js
+++ proyecto_final/myCode.js
@@ -2,6 +2,8 @@ class MainController{
     constructor(){
         this.client = {};
         this.products = [];
+        this.precios = [];
+        this.products_sale = [];
     }
     hello_client(){
         console.log("Hola " + this.client.nombre + " " + this.client.apellido + " cómo estás?");
@@ -13,19 +15,63 @@ class MainController{
 
     add_product(product){
         this.products.push(product)
+        this.precios.push(product.precio);
     }
 
     show_cart_info(){
         let total = 0;
         let msg = "Resumen de compra para " + this.client.nombre + ": \n";
-        for(let i=0; i<this.products.length; i++){
-            total += this.products[i]["precio"];
-            msg += this.products[i]["text"] + "\n"
+        for(const product of this.products){
+            total += product["precio"];
+            msg += product["text"] + "\n"
         }
         msg += "----------------------------------------- \n"
         msg += "Total: $" + total.toString();
         alert(msg);
+        console.log("Detalle de cuenta: " + this.precios.join(" + ") + " = " + total.toString());
     }
+
+    show_cart_info_discounts(){
+        let total = 0;
+        let msg = "Resumen de compra para " + this.client.nombre + ": \n";
+        for(const product of this.products_sale){
+            total += product["precio"];
+            msg += product["text"] + "\n"
+        }
+        msg += "----------------------------------------- \n"
+        msg += "Total: $" + total.toString();
+        alert(msg);
+        console.log("Detalle de cuenta: " + this.precios.join(" + ") + " = " + total.toString());
+    }
+
+    find_by_id(id){
+        const result = this.products.find(elemento => elemento.id === id);
+        if(result != undefined){
+            console.log("Encontrado!!")
+            alert(result.text);
+        }else{
+            console.log("No encontrado!");
+            alert("No encontrado!");
+        }
+    }
+
+    apply_discounts(percentage){
+        this.products_sale = this.products.map(product => ({
+            precio: product.precio * (1 - percentage/100),
+            id: product.id,
+            text: product.text,
+            nombre: product.nombre,
+        }));
+    }
+
+    sort_products(asc = true){
+        if(asc){
+            this.products = this.products.sort(function(a, b){return a.precio - b.precio})
+        }else{
+            this.products = this.products.sort(function(a, b){return b.precio - a.precio})
+        }
+    }
+
 }
 
 class Persona{
@@ -37,7 +83,8 @@ class Persona{
 }
 
 class Product{
-    constructor(text_str){
+    constructor(text_str, id){
+        this.id = id;
         this.text = text_str;
         this.nombre = "";
         this.precio = 0;
@@ -81,7 +128,10 @@ $(function () {
 function restart(){
     request_personal_info();
     request_data_products();
+    mainController.sort_products(false);
     mainController.show_cart_info();
+    find_by_id();
+    apply_discounts();
     //show_cart_info();
 }
 
@@ -107,16 +157,26 @@ function request_data_products(){
 
     for(let i=0; i<3; i++){
         let product = prompt("Ingresa el producto " + i.toString() + " con el siguiente formato: 'Nombre | precio'");
-        product = new Product(product);
+        product = new Product(product, i);
         while(!product.check_format()){
             product = prompt("Ingresa el producto " + i.toString() + " con el siguiente formato: 'Nombre | precio'");
-            product = new Product(product);
+            product = new Product(product, i);
         }
         mainController.add_product(product);
     }
 }
 
 
+function find_by_id(){
+    let id = prompt("Ingresa un id (0, 1, 2) para encontrar un producto!");
+    mainController.find_by_id(parseInt(id));
+}
+
+function apply_discounts(){
+    let percentage = prompt("Ingresa un porcentaje de descuento para aplicar a los productos (0-100)");
+    mainController.apply_discounts(parseFloat(percentage.split("%")[0]));
+    mainController.show_cart_info_discounts();
+}
 
 
 
