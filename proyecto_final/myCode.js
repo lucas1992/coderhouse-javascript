763d1c60261a1c123e7e530cbdd265d11d385e5f
lucas1992 <lucas.etcheverry28@gmail.com>
2021-11-18 01:07:31 -0300

    Entrega 8: Manejo DOM más completo

--- proyecto_final/myCode.js
+++ proyecto_final/myCode.js
@@ -4,18 +4,26 @@ class MainController{
         this.products = [];
         this.precios = [];
         this.products_sale = [];
+        this.products_stock = [];
+        this.total = 0;
     }
     hello_client(){
-        console.log("Hola " + this.client.nombre + " " + this.client.apellido + " cómo estás?");
+        console.log(`Hola ${this.client.nombre} ${this.client.apellido} cómo estás?`);
     }
+
 
     update_client(persona){
         this.client = persona;
     }
 
-    add_product(product){
-        this.products.push(product)
-        this.precios.push(product.precio);
+    add_product(id, i){
+        id = parseInt(id);
+        const product_clone = Object.assign(Object.create(this.products_stock[id]), this.products_stock[id]);
+        product_clone.id = i;
+        this.products.push(product_clone);
+        this.precios.push(product_clone.precio);
+        this.total += product_clone.precio;
+        this.update_total_html();
     }
 
     show_cart_info(){
@@ -25,10 +33,12 @@ class MainController{
             total += product["precio"];
             msg += product["text"] + "\n"
         }
+        this.total = total;
+        this.update_total_html();
         msg += "----------------------------------------- \n"
         msg += "Total: $" + total.toString();
         alert(msg);
-        console.log("Detalle de cuenta: " + this.precios.join(" + ") + " = " + total.toString());
+        console.log(`Detalle de cuenta:  ${this.precios.join(" + ")} = ${total.toString()}`);
     }
 
     show_cart_info_discounts(){
@@ -38,10 +48,12 @@ class MainController{
             total += product["precio"];
             msg += product["text"] + "\n"
         }
+        this.total = total;
+        this.update_total_html();
         msg += "----------------------------------------- \n"
         msg += "Total: $" + total.toString();
         alert(msg);
-        console.log("Detalle de cuenta: " + this.precios.join(" + ") + " = " + total.toString());
+        console.log(`Detalle de cuenta:  ${this.precios.join(" + ")} = ${total.toString()}`);
     }
 
     find_by_id(id){
@@ -70,6 +82,30 @@ class MainController{
         }else{
             this.products = this.products.sort(function(a, b){return b.precio - a.precio})
         }
+    }
+
+    autoload_products(){
+        this.products_stock = [];
+        let products_elements = document.getElementsByClassName("probootstrap-text");
+        let i = 0;
+        for (const product_text of products_elements){
+            let aux = product_text.innerHTML;
+            aux = aux.replace(/[\n\r]/g,'').trim();
+            aux = aux.replace('<h3>','');
+            aux = aux.replace('</h3>','');
+            aux = aux.split(": $");
+            let product = new Product(aux[0] + "|" + aux[1], i);
+            if(product.check_format()){
+                this.products_stock.push(product);
+            }
+            i += 1
+        }
+    }
+
+    update_total_html(){
+        // Agregado entrega 8
+        let div_subtitulo = document.getElementById('subtitulo');
+        div_subtitulo.children[0].children[2].innerHTML = `<h4>Debes $ ${this.total}!</h4>`;
     }
 
 }
@@ -127,13 +163,13 @@ $(function () {
 
 function restart(){
     request_personal_info();
-    /*
+    // Agregado entrega 8: lee automaticamente los productos cargados en el html.
+    mainController.autoload_products();
     request_data_products();
     mainController.sort_products(false);
     mainController.show_cart_info();
     find_by_id();
     apply_discounts();
-    */
     //show_cart_info();
 }
 
@@ -154,25 +190,35 @@ function request_personal_info(){
     // Agregado entrega 8
     let div_subtitulo = document.getElementById('subtitulo');
     let parrafo = document.createElement("p");
-    parrafo.innerHTML = "<h4>¡Hola " + persona.nombre + " " + persona.apellido "!</h4>";
-    parrafo.classList.add('subtitle')
+    parrafo.innerHTML = `<h4>¡Hola ${persona.nombre} ${persona.apellido}!</h4>
+                         <br>
+                         <h4>¡Hola ${persona.nombre} ${persona.apellido}!</h4>`;
+    parrafo.classList.add('subtitle');
     div_subtitulo.appendChild(parrafo);
     console.log(parrafo);
+
+
 }
 
 
 function request_data_products(){
-    alert("En esta primera etapa, con los conocimientos que adquirimos solo puedo solitarte que ingreses los productos via prompt:");
-    alert("A continuacion te solicitare que ingreses 3 productos con el siguiente formato: 'Nombre | precio'");
+    alert("A continuacion te solicitare que ingreses los ID de 3 productos. El listado completo de los mismos sera brindado a continuación:");
+    ids_allowed = {}
+    text_products = "";
+    for(const product of mainController.products_stock){
+        text_products += `ID: ${product.id.toString()}  -->  ${product.nombre} $ ${product.precio} \n`;
+        ids_allowed[product.id.toString()] = "";
+    }
+
+    alert(text_products);
+    console.log(text_products);
 
     for(let i=0; i<3; i++){
-        let product = prompt("Ingresa el producto " + i.toString() + " con el siguiente formato: 'Nombre | precio'");
-        product = new Product(product, i);
-        while(!product.check_format()){
-            product = prompt("Ingresa el producto " + i.toString() + " con el siguiente formato: 'Nombre | precio'");
-            product = new Product(product, i);
+        let id = prompt("Ingresa el id para el producto " + i.toString() );
+        while(!(id in ids_allowed)){
+            id = prompt("No existe producto con dicho ID. Ingresa nuevamente el id para el producto " + i.toString() );
         }
-        mainController.add_product(product);
+        mainController.add_product(id, i);
     }
 }
 
@@ -187,7 +233,6 @@ function apply_discounts(){
     mainController.apply_discounts(parseFloat(percentage.split("%")[0]));
     mainController.show_cart_info_discounts();
 }
-
 
 
 
